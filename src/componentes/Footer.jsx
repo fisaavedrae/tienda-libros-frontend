bb7448e79fa33dc63fcae150988cf99c67bb4b8c
Felipe Saavedra <fisaavedrae@gmail.com>
2024-03-11 15:00:01 -0300

    asd

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -7,7 +7,8 @@ const Footer = (props) => {
       <footer className="py-3 my-4">
         <ul className="nav justify-content-center border-bottom pb-3 mb-3"></ul>
         <p className="text-center text-body-secondary">
-          © 2024 Desarrollado por Ricardo Barrientos y{" "}
+          © 2024 Desarrollado por{" "}
+          <a href="https://github.com/rimar-basaa">Ricardo Barrientos</a> y{" "}
           <a href="https://fidatech.net/felipe">Felipe Saavedra</a>
         </p>
       </footer>
