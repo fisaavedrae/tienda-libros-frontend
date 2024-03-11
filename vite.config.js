81d2ce66078e5aff24d63ea3bfbb3df26fb3cc4e
Felipe Saavedra <fisaavedrae@gmail.com>
2024-03-11 14:36:46 -0300

    asd

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-import { defineConfig } from "vite";
-import react from "./node_modules/@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["react", "react-router", "react-router-dom"],
-      output: {
-        globals: {
-          react: "React",
-        },
-      },
-    },
-  },
-});
+})
