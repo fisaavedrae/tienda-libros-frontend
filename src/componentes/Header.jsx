fd14c503f97860cf8147cedfa4e3216fe0f197aa
Felipe Saavedra <fisaavedrae@gmail.com>
2024-03-12 14:24:36 -0300

    link a admin desde header

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -26,36 +26,75 @@ const Header = (props) => {
             </button>
             <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
               <div className="d-flex flex-row justify-content-end items-end gap-3 w-100">
-                <Link to="/">
-                  <i className="fa-solid fa-house"></i>
-                </Link>
-                <i
-                  className="fa-solid fa-magnifying-glass"
-                  data-bs-toggle="offcanvas"
-                  data-bs-target="#offcanvasSearch"
-                  aria-controls="offcanvasSearch"
-                ></i>
+                <ul className="nav justify-content-end gap-3">
+                  <li className="nav-item">
+                    <Link to="/">
+                      <i className="fa-solid fa-house"></i>
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <i
+                      className="fa-solid fa-magnifying-glass"
+                      data-bs-toggle="offcanvas"
+                      data-bs-target="#offcanvasSearch"
+                      aria-controls="offcanvasSearch"
+                    ></i>
+                  </li>
+                  <li className="nav-item">
+                    <i
+                      className="fa-solid fa-cart-shopping position-relative icono-header"
+                      data-bs-toggle="offcanvas"
+                      data-bs-target="#offcanvasExample"
+                      aria-controls="offcanvasExample"
+                    >
+                      <span
+                        id="circuloCarro"
+                        className="position-absolute top-0 start-100 translate-middle p-1  border border-light rounded-circle"
+                      >
+                        {carro.length}
+                        <span className="visually-hidden">
+                          productos en el carro
+                        </span>
+                      </span>
+                    </i>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/Admin">
+                      <i className="fa-solid fa-gear"></i>
+                    </Link>
+                  </li>
 
-                <i
-                  className="fa-solid fa-cart-shopping position-relative icono-header"
-                  data-bs-toggle="offcanvas"
-                  data-bs-target="#offcanvasExample"
-                  aria-controls="offcanvasExample"
-                >
-                  <span
-                    id="circuloCarro"
-                    className="position-absolute top-0 start-100 translate-middle p-1  border border-light rounded-circle"
-                  >
-                    {carro.length}
-                    <span className="visually-hidden">
-                      productos en el carro
-                    </span>
-                  </span>
-                </i>
-                <Link to="/Admin">
-                  <i className="fa-solid fa-gear"></i>
-                </Link>
-                <i className="fa-solid fa-user"></i>
+                  <li className="nav-item dropdown">
+                    <i
+                      className="fa-solid fa-user "
+                      data-bs-toggle="dropdown"
+                      data-bs-display="static"
+                      aria-expanded="false"
+                    ></i>
+                    <ul className="dropdown-menu dropdown-menu-end ">
+                      <li>
+                        <Link className="dropdown-item" to={"/Login"}>
+                          Login
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item" to={"/Registro"}>
+                          Crear cuenta
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item" to={"/Ordenes"}>
+                          Mis Ordenes
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item" to={"/CerrarSesion"}>
+                          Cerrar Sesion
+                        </Link>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
